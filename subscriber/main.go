0892a5ab147baa5798c75cab0282b80d18ed370d
erry-azh <erry.azhari@tokopedia.com>
2021-06-25 08:16:45 +0700

    update git ignore

---
+++ subscriber/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	mqtt.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	mqtt.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
+	mqtt.WARN = log.New(os.Stdout, "[WARN]  ", 0)
+	mqtt.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+
+}
